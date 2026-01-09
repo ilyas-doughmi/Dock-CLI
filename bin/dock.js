ef506f3ccf69a8eb4c5e997d6bae436e82e6ead2
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-10 00:09:48 +0100

    feat: Add 'test' command to CLI with success logging

--- bin/dock.js
+++ bin/dock.js
@@ -7,4 +7,11 @@ const program = new Command();
 program
   .version('1.0.0')
   .description('Dock Hosting - CLI');
+
+program
+    .command('test')
+    .description('testing cli')
+    .action(()=>{
+        logger.success("All Good");
+    })
 program.parse(process.argv);
