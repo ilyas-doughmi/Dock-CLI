7dfc42d3c3597d76e5e94e8bc2454e0e926c2c10
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-10 00:57:52 +0100

    feat: setuping auth.js for user login functionality

---
+++ src/lib/auth.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import open from 'open';
+import config from '../utils/config';
+
+export const login = () => {
+    return new Promise((resolve,reject) => {
+        const app = express();
+    })
+}
