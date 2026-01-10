9cb2c9ace67df416b7bda8a61243bc7fd517e7bf
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-10 10:55:12 +0100

    feat: implement project deployment via CLI

--- src/lib/api.js
+++ src/lib/api.js
@@ -7,9 +7,35 @@ import path from 'path';
 const API_URL = 'http://localhost/dock-hosting/api';
 
 export const deploy = async (zipFilePath) => {
-    const token = config.get('auth_token');
+    const token = config.get('auth.token');
 
     if(!token){
         throw new Error('login first');
+    }
+
+    const form = new FormData();
+    form.append('token', token);
+
+    const projectName = path.basename(process.cwd());
+    form.append('project_name', projectName);
+    
+    form.append('project_zip', fs.createReadStream(zipFilePath));
+
+
+    try{
+        const response = await axios.post(`${API_URL}/cli_deploy.php`,form,{
+            headers: {
+                ...form.getHeaders(),
+            },
+            maxBodyLength: Infinity,
+            maxContentLength: Infinity
+        });
+
+        return response.data;
+    }catch(error){
+        if(error.response){
+            throw new Error(`server problem ${error.message}`);
+        }
+        throw error;
     }
 }
