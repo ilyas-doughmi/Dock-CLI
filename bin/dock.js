3508db5cec2870ffbeeaa3352f3623cc8028ed74
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-10 01:41:43 +0100

    feat: add login command to CLI for user authentication

--- bin/dock.js
+++ bin/dock.js
@@ -2,6 +2,7 @@
 
 import { Command } from 'commander';
 import * as logger from '../src/utils/logger.js';
+import loginCommand from '../src/commands/login.js';
 const program = new Command();
 
 program
@@ -9,9 +10,9 @@ program
   .description('Dock Hosting - CLI');
 
 program
-    .command('test')
-    .description('testing cli')
-    .action(()=>{
-        logger.success("All Good");
+    .command('login')
+    .description('Login to your Dock Hosting account')
+    .action(async ()=>{
+        await loginCommand();
     })
 program.parse(process.argv);
