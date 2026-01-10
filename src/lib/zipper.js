8f30be554019823581a1b7993c6518a858b2d180
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-10 10:37:19 +0100

    feat: implement zipDirectory logic using archiver

--- src/lib/zipper.js
+++ src/lib/zipper.js
@@ -8,5 +8,23 @@ export const zipDirectory = () =>{
         const outputPath = path.join(process.cwd(), 'project.zip');
         const output = fs.createWriteStream(outputPath);
 
+        const archive = archiver('zip', {zlib: {level: 9} });
+
+        output.on('close', () => {
+            resolve(outputPath);
+        })
+
+        archive.on('error',(err)=>{
+            reject(err);
+        })
+
+        archive.pipe(output);
+
+        archive.glob('**/*', {
+            cwd: process.cwd(),
+            ignore: ['node_modules/**','.git/**', 'project.zip', '.env']
+        })
+
+        archive.finalize();
     })
 }
