61a68bd68c932f81629ad7d4736087fed80c3b8b
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-10 11:02:23 +0100

    feat: add deploy command to CLI for file deployment

--- bin/dock.js
+++ bin/dock.js
@@ -3,6 +3,7 @@
 import { Command } from 'commander';
 import * as logger from '../src/utils/logger.js';
 import loginCommand from '../src/commands/login.js';
+import deployCommand from '../src/commands/deploy.js';
 const program = new Command();
 
 program
@@ -14,5 +15,11 @@ program
     .description('Login to your Dock Hosting account')
     .action(async ()=>{
         await loginCommand();
+    })
+program
+    .command('deploy')
+    .description('Deploy your files easly with deploy command')
+    .action( async () =>{
+        await deployCommand();
     })
 program.parse(process.argv);
