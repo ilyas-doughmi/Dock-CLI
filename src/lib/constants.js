cd4da6548cc675a489c9a96b91d3169291cf192b
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-07 18:16:01 +0100

    Add constants module

---
+++ src/lib/constants.js
@@ -0,0 +1,7 @@
+const API_URL = process.env.DOCK_API_URL || 'http://localhost:8080/api';
+const WEB_URL = process.env.DOCK_WEB_URL || 'http://localhost:3000';
+
+module.exports = {
+    API_URL,
+    WEB_URL
+};
